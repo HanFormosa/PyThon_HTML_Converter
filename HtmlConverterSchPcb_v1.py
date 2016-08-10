c6961821f2139dd20811fb5bcf7b9b6d56453529
HanPerGmail <tzehan.su@gmail.com>
2016-08-10 23:04:59 +0800

    added new script for schematics layout html converter. But seems only useful for MA725F. maybe 1 model 1 script?

---
+++ HtmlConverterSchPcb_v1.py
@@ -0,0 +1,187 @@
+from tkinter import *
+from tkinter import filedialog
+#from Tkinter import * #try this if in terminal don't work
+
+root = Tk()
+
+# ****** constants *******
+kVERSION = 0
+kBOOTLOADER = 1
+kRELEASE = 2
+
+
+def selectBoard(x):
+    entry_board.delete(0, END)  # clear text box widget
+    if x == 1:
+        entry_board.insert(0, "1.MAIN POWER")
+    elif x == 2:
+        entry_board.insert(0, "2.POWER AMP")
+    elif x == 3:
+        entry_board.insert(0, "3.POWER AMP")
+    elif x == 4:
+        entry_board.insert(0, "4.POWER AMP")
+    elif x == 5:
+        entry_board.insert(0, "5.POWER AMP")
+    elif x == 6:
+        entry_board.insert(0, "6.POWER AMP")
+    elif x == 7:
+        entry_board.insert(0, "7.POWER AMP")
+    elif x == 8:
+        entry_board.insert(0, "8.POWER AMP")
+    else:
+        entry_board.insert(0, "9.POWER SW")
+
+def doGenerate():
+    text_Converted.delete(1.0, END) #clear text box widget
+    str_modelName = entry_modelName.get()
+
+    str_version = entry_version.get()
+    str_version_sub1 = entry_version_sub1.get()
+
+    str_Date = entry_Date.get()
+
+    str_board = entry_board.get()
+
+    str_rel = entry_rel.get()
+    str_rel_sub1 = entry_rel_sub1.get()
+
+    tr_line = "  <tr>\n"
+    td_line1 = "\t<td><a href=\"" + str_modelName + "/" + str_board + "/" + str_version_sub1 + "\" download>" + str_version + "</a></td>\n"
+    td_line2 = "\t<td><a href=\"" + str_modelName + "/" + str_board + "/" + str_rel_sub1 + "\" target=_blank>" + str_rel + "</a></td>\n"
+    td_line3 = "\t<td>" + str_Date + "</td>\n"
+    tr_end_line = "  </tr>\n"
+
+    final_str = tr_line + td_line1 + td_line3 + td_line2 + tr_end_line
+
+    text_Converted.insert(END, final_str)
+
+def browseVersion():
+    file_path = filedialog.askopenfilename()
+    entry_version_sub1.delete(0, END)
+    # add some magic to display only file name, by detecting the last "/"
+    numberOfStrokes = file_path.count('/') # find total number of strokes, used this in for loop later
+    strokeCount = 0
+    for x in range(0, len(file_path)):
+        str_temp = file_path[x]
+        # iterate through each characters, and count up once a stroke is found.
+        if str_temp is '/':
+            strokeCount += 1
+            # when number of strokes count reaches the expected total stroke counts,this means this is the last stroke in the string.
+            if strokeCount == numberOfStrokes:
+                # extract the strings from the last stroke till end of file, to get the file name only
+                fileName = file_path[x+1:]
+                entry_version_sub1.insert(0, fileName)
+                break
+    #if fileType == kVERSION:
+    #    entry_version_sub1.insert(0, fileName)  # display text in entry box
+    #elif fileType == kBOOTLOADER:
+    #    entry_BL_sub1.insert(0, fileName)
+    #elif fileType == kRELEASE:
+    #    entry_rel_sub1.insert(0,fileName)
+    #else:
+    #    print("E: did not specify which text box")
+
+def browseRel():
+    file_path = filedialog.askopenfilename()
+    entry_rel_sub1.delete(0, END)
+    # add some magic to display only file name, by detecting the last "/"
+    numberOfStrokes = file_path.count('/')  # find total number of strokes, used this in for loop later
+    strokeCount = 0
+    for x in range(0, len(file_path)):
+        str_temp = file_path[x]
+        # iterate through each characters, and count up once a stroke is found.
+        if str_temp is '/':
+            strokeCount += 1
+            # when number of strokes count reaches the expected total stroke counts,this means this is the last stroke in the string.
+            if strokeCount == numberOfStrokes:
+                # extract the strings from the last stroke till end of file, to get the file name only
+                fileName = file_path[x + 1:]
+                entry_rel_sub1.insert(0, fileName)
+                break
+
+# ******** label *********
+
+label_modelName = Label(text="Model Name")
+label_version = Label(text="Version")
+label_Date = Label(text="Date")
+label_rel = Label(text="Release Note")
+
+label_fileName = Label(text="File Name")
+
+label_board = Label(text="Board")
+
+# ****** user input textbox *******
+
+entry_modelName = Entry(root)
+entry_modelName.insert(0, "MODEL")
+
+entry_version = Entry(root)
+entry_version.insert(0, "1.0")
+entry_version_sub1 = Entry(root)
+
+entry_Date = Entry(root)
+entry_Date.insert(0, "2016-mm-dd")
+
+
+entry_rel = Entry(root)
+entry_rel.insert(0, "Release Note")
+entry_rel_sub1 = Entry(root)
+
+entry_board = Entry(root)
+entry_board.insert(0, "1.MAIN POWER")
+
+# ****** Text Box *********
+text_Converted = Text(root)
+
+# *******Buttons ******* TODO: add command
+button_BrowseVersion = Button(text="...", command=browseVersion)
+button_BrowseRel = Button(text="...", command=browseRel)
+button_Generate = Button(text="Generate", command=doGenerate)
+
+# -- radio button  --
+v = IntVar()
+
+Radiobutton(root, text="1.MAIN POWER", variable=v, value=1, command=lambda: selectBoard(1)).grid(row=1, column=1)
+Radiobutton(root, text="2.POWER AMP", variable=v, value=2, command=lambda: selectBoard(2)).grid(row=1, column=2)
+Radiobutton(root, text="3.POWER AMP", variable=v, value=3, command=lambda: selectBoard(3)).grid(row=1, column=3)
+
+Radiobutton(root, text="4.POWER AMP", variable=v, value=4, command=lambda: selectBoard(4)).grid(row=2, column=1)
+Radiobutton(root, text="5.POWER AMP", variable=v, value=5, command=lambda: selectBoard(5)).grid(row=2, column=2)
+Radiobutton(root, text="6.POWER AMP", variable=v, value=6, command=lambda: selectBoard(6)).grid(row=2, column=3)
+
+Radiobutton(root, text="7.POWER AMP", variable=v, value=7, command=lambda: selectBoard(7)).grid(row=3, column=1)
+Radiobutton(root, text="8.POWER AMP", variable=v, value=8, command=lambda: selectBoard(8)).grid(row=3, column=2)
+Radiobutton(root, text="9.POWER SW", variable=v, value=9, command=lambda: selectBoard(9)).grid(row=3, column=3)
+
+# ****** positioning in grid *********
+
+label_modelName.grid(row=0, sticky=E)
+entry_modelName.grid(row=0, column=1)
+
+label_board.grid(row=1, sticky=E)
+
+entry_board.grid(row=4,column=1)
+
+label_fileName.grid(row=4, column=2)
+
+label_version.grid(row=5, sticky=E)
+entry_version.grid(row=5, column=1)
+entry_version_sub1.grid(row=5, column=2)
+button_BrowseVersion.grid(row=5, column=3)
+
+label_Date.grid(row=6, sticky=E)
+entry_Date.grid(row=6, column=1)
+
+
+label_rel.grid(row=7, sticky=E)
+entry_rel.grid(row=7, column=1)
+entry_rel_sub1.grid(row=7, column=2)
+button_BrowseRel.grid(row=7, column=3)
+
+button_Generate.grid(row=8, column=1, columnspan=2)
+
+text_Converted.grid(row=9, column=0, columnspan=4)
+
+
+root.mainloop()
+
