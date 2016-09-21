c32944263edfcecfa2de57eb1a3874fcd37d9d04
HanPersonal <tzehan_su@hotmail.com>
2016-09-21 22:18:14 +0800

    adjustment of repeat count. need to find the correct value.

--- 1701_Converter/1701_Converter_v2.py
+++ 1701_Converter/1701_Converter_v2.py
@@ -3,8 +3,8 @@ from sys import argv
 script, filename = argv
 
 # ******* constants **********
-kPROGRAM = "ADI_REG_TYPE Program_Data"
-kRepeatPROGRAM = "0x00, 0x00, 0x00, 0x00, 0x01,"
+kPROGRAM = "ADI_REG_TYPE Param_Data"
+kRepeatPROGRAM = "0x00, 0x00, 0x00, 0x00,"
 
 # ADI_REG_TYPE Param_Data
 # 0x00, 0x00, 0x00, 0x00,
@@ -45,6 +45,8 @@ else:
     # find repeat start, and find iTo
     myFile = open(filename, "r")
     iPrev = 0
+    iDiffCount = 0
+    repeatCount = 10 # how many times repetition considered as repetition
     for i, line in enumerate(myFile):
         if i >= int(iFrom) - 1 and i < int(iParenthesis):
             if kRepeatPROGRAM in line:
@@ -52,9 +54,13 @@ else:
                 iDiff = i+1 - iPrev
                 if iDiff == 1:
                     print("{0} found at line before repeat at line {1}".format(line, i-1))
+                    iDiffCount += 1 # increment for repeating same line
+                # if more than 4 lines repeating
+                if iDiffCount > repeatCount:
+                    print("{0} found at 4 lines after repeat at line {1}".format(line, i - 1))
                     break
                 iPrev = i+1
-    iTo = i-1
+    iTo = i-1-repeatCount
     print("iTo is {0}".format(iTo))
     myFile.close()
 
