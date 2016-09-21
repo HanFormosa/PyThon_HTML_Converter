8a6df21981d68b1b43c4e89712f5a68da5069cbb
HanPersonal <tzehan_su@hotmail.com>
2016-09-21 19:32:43 +0800

    find iTo by using the line before repetition starts

--- 1701_Converter/1701_Converter_v2.py
+++ 1701_Converter/1701_Converter_v2.py
@@ -14,7 +14,7 @@ else:
             print("{0} found at line {1}".format(line, i+1))
             break
     iFrom = i + 2 # offset is 2
-    iTo = 48 # TODO: remove when iTo is done
+    # iTo = 48 # TODO: remove when iTo is done
     myFile.close() # so that the next enumeration won't continue adding up
 
     # find iParenthesis (end of program data)
@@ -26,20 +26,23 @@ else:
                 break
 
     iParenthesis = i + 1
+    print("iParenthesis is {0}".format(iParenthesis))
     myFile.close()
 
-    # find repeat start
+    # find repeat start, and find iTo
     myFile = open(filename, "r")
     iPrev = 0
     for i, line in enumerate(myFile):
-        if "0x00, 0x00, 0x00, 0x00, 0x01," in line:
-            print("{0} found at line {1}".format(line, i+1))
-            iDiff = i+1 - iPrev
-            if iDiff == 1:
-                print("{0} found line before repeat at line {1}".format(line, i-1))
-                break
-            iPrev = i+1
-    # iParenthesis = i
+        if i >= int(iFrom) - 1 and i < int(iParenthesis):
+            if "0x00, 0x00, 0x00, 0x00, 0x01," in line:
+                # print("{0} found at line {1}".format(line, i+1))
+                iDiff = i+1 - iPrev
+                if iDiff == 1:
+                    print("{0} found at line before repeat at line {1}".format(line, i-1))
+                    break
+                iPrev = i+1
+    iTo = i-1
+    print("iTo is {0}".format(iTo))
     myFile.close()
 
 
