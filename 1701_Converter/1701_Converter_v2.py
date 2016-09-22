eae8dacb95a51dfa7ba8b8f94a5e52f88c49d705
HanPerGmail <tzehan.su@gmail.com>
2016-09-22 23:36:13 +0800

    now possible to extract from 3 choices: 1. Program data 2. Param Data 3. HW config

--- 1701_Converter/1701_Converter_v2.py
+++ 1701_Converter/1701_Converter_v2.py
@@ -13,21 +13,22 @@ kRepeatPROGRAM = "0x00, 0x00, 0x00, 0x00, 0x01,"
 kPARAMETER = "ADI_REG_TYPE Param_Data"
 kRepeatPARAM = "0x00, 0x00, 0x00, 0x00,"
 
-kHW_CONFIG = "R3_HWCONFIGURATION"
+kHW_CONFIG = "ADI_REG_TYPE R3_HWCONFIGURATION"
 
 
-def extractPROGRAM_PARAMETER(dataType):
+def extractPROGRAM_PARAMETER_HWCONFIG(dataType):
     varType = int(dataType)  # depend on user selection, default is zero
     if varType == kkPROGRAM:
-        writeFileName = "PROGRAM.txt"
+        writeFileName = "_PROGRAM.tmp"
         varTypeText = kPROGRAM
         varTypeTextRepeat = kRepeatPROGRAM
     elif varType == kkPARAM:
-        writeFileName = "PARAM.txt"
+        writeFileName = "_PARAM.tmp"
         varTypeText = kPARAMETER
         varTypeTextRepeat = kRepeatPARAM
     elif varType == kkHW:
-        writeFileName = "HWCONFIG.txt"
+        writeFileName = "_HWCONFIG.tmp"
+        varTypeText = kHW_CONFIG
 
     try:
         myFile = open(filename, "r")
@@ -56,32 +57,35 @@ def extractPROGRAM_PARAMETER(dataType):
         myFile.close()
 
         # find repeat start, and find iTo
-        myFile = open(filename, "r")
-        iPrev = 0
-        iDiffCount = 0
-        iFlag = 0
-        repeatCount = 6  # how many times repetition considered as repetition
-        for i, line in enumerate(myFile):
-            if i >= int(iFrom) - 1 and i < int(iParenthesis):
-                if varTypeTextRepeat in line:
-                    # print("{0} found at line {1}".format(line, i+1))
-                    iDiff = i + 1 - iPrev
-                    if iFlag == 1:  # "watch out for repeat" flag to catch continuous repeat
-                        if iDiff != 1:
-                            iDiffCount = 0  # clear repeat counter
-                            iFlag = 0  # clear "watch out for repeat" flag
-                    if iDiff == 1:
-                        # print("{0} found at line before repeat at line {1}".format(line, i-1))
-                        iDiffCount += 1  # increment for repeating same line
-                        iFlag = 1
-                    # if more than 4 lines repeating
-                    if iDiffCount > repeatCount:
-                        print("{0} found at 10 lines after repeat at line {1}".format(line, i - 1))
-                        break
-                    iPrev = i + 1
-        iTo = i - 1 - repeatCount
-        print("iTo is -{0} lines, {1}".format(repeatCount, iTo))
-        myFile.close()
+        if varType != kkHW:  # HW config no need to find repetition, so skip through this
+            myFile = open(filename, "r")
+            iPrev = 0
+            iDiffCount = 0
+            iFlag = 0
+            repeatCount = 6  # how many times repetition considered as repetition
+            for i, line in enumerate(myFile):
+                if i >= int(iFrom) - 1 and i < int(iParenthesis):
+                    if varTypeTextRepeat in line:
+                        # print("{0} found at line {1}".format(line, i+1))
+                        iDiff = i + 1 - iPrev
+                        if iFlag == 1:  # "watch out for repeat" flag to catch continuous repeat
+                            if iDiff != 1:
+                                iDiffCount = 0  # clear repeat counter
+                                iFlag = 0  # clear "watch out for repeat" flag
+                        if iDiff == 1:
+                            # print("{0} found at line before repeat at line {1}".format(line, i-1))
+                            iDiffCount += 1  # increment for repeating same line
+                            iFlag = 1
+                        # if more than 4 lines repeating
+                        if iDiffCount > repeatCount:
+                            print("{0} found at 10 lines after repeat at line {1}".format(line, i - 1))
+                            break
+                        iPrev = i + 1
+            iTo = i - 1 - repeatCount
+            print("iTo is -{0} lines, {1}".format(repeatCount, iTo))
+            myFile.close()
+        else:  # this is if equal to HW_CONFIG, assign iTo to line before Parenthesis
+            iTo = iParenthesis - 1
 
         # extract by line number
         myFile = open(filename, "r")
@@ -94,10 +98,10 @@ def extractPROGRAM_PARAMETER(dataType):
                 break
 
         # file size is just total number of lines
-        fileSize = iTo - iFrom  # TODO: confirm is need to + 1, line number count starting with 1 or 0
+        fileSize = iTo - iFrom + 1  # TODO: confirm is need to + 1, line number count starting with 1 or 0
         print("File Size is {0}".format(fileSize))
         myFile.close()
         writeFile.close()
 
-extractPROGRAM_PARAMETER(dataType)
+extractPROGRAM_PARAMETER_HWCONFIG(dataType)
 
